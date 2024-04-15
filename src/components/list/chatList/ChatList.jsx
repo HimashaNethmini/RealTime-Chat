f01cc871b34b0293b26c32ff1c1f91998da1fe5d
HimashaNethmini <himashanethmini05@gmail.com>
2024-04-15 13:50:10 +0530

    added search bar

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -3,7 +3,19 @@ import "./chatList.css";
 
 const ChatList = () => {
   return (
-    <div className='chatList'>ChatList</div>
+    <div className='chatList'>
+
+      {/* search container */}
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder='Search'/>
+        </div>
+        
+        <img src="./plus.png" alt="" />
+      </div>
+
+    </div>
   )
 }
 
