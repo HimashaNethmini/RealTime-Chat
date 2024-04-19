459d1dab9a31bf30ef861c6eb5c394ba9c580b48
HimashaNethmini <himashanethmini05@gmail.com>
2024-04-19 19:26:20 +0530

    making the chat side

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -3,7 +3,23 @@ import "./chat.css"
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+
+    <div className='chat'>
+      <div className="top">
+        <div className="user">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+              <span>Ariana Grande</span>
+              <p>bjkfuqhfu HAULDUIQWDIUQIJDQDJade ejfiejlf jdiijewfjwejfj</p>
+            </div>
+          </div>
+        
+      <div className="icons"></div>
+      </div>
+
+      <div className="center"></div>
+      <div className="buttom"></div>
+    </div>
   )
 }
 
