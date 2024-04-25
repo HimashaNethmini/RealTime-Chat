441d1870fb1f1655039a731eee4f6ad0bfcedd2d
HimashaNethmini <himashanethmini05@gmail.com>
2024-04-25 22:24:23 +0530

    add the chat msg part

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-import "./chat.css"
+import React from "react";
+import "./chat.css";
 
 const Chat = () => {
   return (
-
-    <div className='chat'>
+    <div className="chat">
       <div className="top">
         <div className="user">
-            <img src="./avatar.png" alt="" />
-            <div className="texts">
-              <span>Ariana Grande</span>
-              <p>bjkfuqhfu HAULDUIQWiijewfjwejfj</p>
-            </div>
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Ariana Grande</span>
+            <p>bjkfuqhfu HAULDUIQWiijewfjwejfj</p>
           </div>
-{/* make the icon section of chat part */}
-      <div className="icons">
-        <img src="./phone.png" alt="" />
-        <img src="./video.png" alt="" />
-        <img src="./info.png" alt="" />
-      </div>
+        </div>
+        {/* make the icon section of chat part */}
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
       </div>
 
       <div className="center"></div>
-      <div className="buttom"></div>
+      <div className="buttom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input type="text" placeholder="Type a message...." />
+        <div className="emoji">
+          <img src="./emoji.png" alt="" />
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
