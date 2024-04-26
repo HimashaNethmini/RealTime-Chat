222de0b639a68581534f5b5a2ecb36867872faa0
HimashaNethmini <himashanethmini05@gmail.com>
2024-04-26 10:32:54 +0530

    added clear space inbetween name and icons

--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -3,15 +3,13 @@ import "./userInfo.css";
 
 const Userinfo = () => {
   return (
-
     <div className="userInfo">
-      {/* user image n name section */}
+
       <div className="user">
         <img src="./avatar.png" alt="" />
-        <h2>Ariana Grande</h2>
+        <h2 style={{ marginRight: "20px", marginTop:"-5px" }}>John Doe</h2>
       </div>
 
-      {/* section */}
       <div className="icons">
         <img src="./more.png" alt="" />
         <img src="./video.png" alt="" />
