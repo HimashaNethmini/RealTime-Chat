70cf0ac500834ba765890730fafb0b03216d277b
HimashaNethmini <himashanethmini05@gmail.com>
2024-04-27 19:45:29 +0530

    make the chat component

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -8,31 +8,18 @@ const Chat = () => {
         <div className="user">
           <img src="./avatar.png" alt="" />
           <div className="texts">
-            <span>Ariana Grande</span>
-            <p>bjkfuqhfu HAULDUIQWiijewfjwejfj</p>
+            <span>John Doe</span>
+            <p>Hello</p>
           </div>
         </div>
-        {/* make the icon section of chat part */}
         <div className="icons">
           <img src="./phone.png" alt="" />
           <img src="./video.png" alt="" />
-          <img src="./info.png" alt="" />
+          <img src="info.png" alt="" />
         </div>
       </div>
-
       <div className="center"></div>
-      <div className="buttom">
-        <div className="icons">
-          <img src="./img.png" alt="" />
-          <img src="./camera.png" alt="" />
-          <img src="./mic.png" alt="" />
-        </div>
-        <input type="text" placeholder="Type a message...." />
-        <div className="emoji">
-          <img src="./emoji.png" alt="" />
-        </div>
-        <button className="sendButton">Send</button>
-      </div>
+      <div className="bottom"></div>
     </div>
   );
 };
