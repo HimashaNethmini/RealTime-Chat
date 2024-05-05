2746838bb5a36e071ffdb48b8e8488dc6c3126a4
HimashaNethmini <himashanethmini05@gmail.com>
2024-05-05 19:01:04 +0530

    handle the emoji function

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
+  // for open and close of the emoji picker
+  const [open, setOpen] = useState(false);
+  //to handle the emoji function
+  const [text, setText] = useState("");
+
+  //function to have the emoji on the text box
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false)
+  };
+
   return (
     <div className="chat">
       <div className="top">
@@ -29,8 +41,15 @@ const Chat = () => {
           <img src="./mic.png" alt="" />
         </div>
         <input type="text" placeholder="Type a message ......" />
+
+        {/* adding the emojiPicker library with useState*/}
         <div className="emoji">
-          <img src="./emoji.png" alt="" />
+          <img src="./emoji.png" 
+            alt="" 
+            onClick={() => setOpen ((prev) => !prev)} />
+
+            {/* giving a function called handleEmoji  */}
+          <EmojiPicker open={open}  onEmojiClick={handleEmoji}/>
         </div>
         <button className="sendButton">Send</button>
       </div>
