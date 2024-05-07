4937d368991a6efa81496e9cc817d53c9cb55a76
HimashaNethmini <himashanethmini05@gmail.com>
2024-05-07 16:38:12 +0530

    added dummy texts

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -38,10 +38,6 @@ const Chat = () => {
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic
             </p>
             <span>1 min ago</span>
           </div>
@@ -51,11 +47,6 @@ const Chat = () => {
           <div className="texts">
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic
             </p>
             <span>1 min ago</span>
           </div>
@@ -67,10 +58,6 @@ const Chat = () => {
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic
             </p>
             <span>1 min ago</span>
           </div>
@@ -80,11 +67,6 @@ const Chat = () => {
           <div className="texts">
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic
             </p>
             <span>1 min ago</span>
           </div>
