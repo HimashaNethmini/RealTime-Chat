87faf7ccc5ecd5dc141a6d41efe1f00253a3317a
HimashaNethmini <himashanethmini05@gmail.com>
2024-05-09 11:22:26 +0530

    completed the detail page

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import "./detail.css"
+import React from "react";
+import "./detail.css";
 
 const Detail = () => {
   return (
-    <div className='detail'>
+    <div className="detail">
       <div className="user">
         <img src="./avatar.png" alt="" />
         <h2>John Doe</h2>
@@ -31,23 +31,26 @@ const Detail = () => {
           </div>
           <div className="photos">
             <div className="photoItem">
-              <img src="https://images.pexels.com/photos/3419719/pexels-photo-3419719.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+              <img
+                src="https://images.pexels.com/photos/3419719/pexels-photo-3419719.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
               <span>photo_2024_2.png</span>
             </div>
             <img src="./download.png" alt="" />
           </div>
         </div>
 
-      <div className="option">
-        <div className="title">
-          <span>Shared Files</span>
-          <img src="./arrowUp.png" alt="" />
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
         </div>
-      </div>
-      <button>Block User</button>
+        <button>Block User</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
