4dbf8dd432463f27dff588a82e0fe410228dc270
HimashaNethmini <himashanethmini05@gmail.com>
2024-05-10 09:21:36 +0530

    if there's a user, call the components, otherwise ask to log in

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import List from "../src/components/list/List";
 import Chat from "../src/components/chat/Chat";
 import Detail from "../src/components/detail/Detail";
+import Login from "./components/login/Login";
 
 const App = () => {
+  
+  //if there's a user, call the components, otherwise ask to log in
   return (
-    <div className='container'>
-      <List />
-      <Chat />
-      <Detail/>
+    <div className="container">
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
