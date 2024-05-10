0359e838cb6a12d9900c5be3923ff68b9d2f5ab3
HimashaNethmini <himashanethmini05@gmail.com>
2024-05-10 09:30:09 +0530

    create the login, signup pages

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
-import "./login.css"
+import React from "react";
+import "./login.css";
 
 const login = () => {
   return (
-    <div className='login'>
+    <div className="login">
       <div className="item">
         <h2>Welcome back,</h2>
         {/* login form */}
+        <form>
+          <input type="text" placeholder="Username" name="username" />
+          <input type="text" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button>Sign In</button>
+        </form>
       </div>
 
-
-
       <div className="separator"></div>
-      <div className="item"></div>
-    </div>
-  )
-}
 
-export default login
+      <div className="item">
+        <h2>Create an Account</h2>
+        <form>
+          <input type="file" id="file" />
+          <input type="text" placeholder="Username" name="username" />
+          <input type="text" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default login;
