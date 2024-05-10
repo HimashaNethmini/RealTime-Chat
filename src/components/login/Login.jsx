95ca2aa7b985385b2979140ef8dc3e4e0ced7b51
HimashaNethmini <himashanethmini05@gmail.com>
2024-05-10 09:24:32 +0530

    make the login page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import "./login.css"
+
 const login = () => {
   return (
-    <div>login</div>
+    <div className='login'>
+      <div className="item">
+        <h2>Welcome back,</h2>
+        {/* login form */}
+      </div>
+
+
+
+      <div className="separator"></div>
+      <div className="item"></div>
+    </div>
   )
 }
 
