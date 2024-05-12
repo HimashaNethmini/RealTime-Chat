1bcc3f325b06abbd011587b499798db50db9495a
HimashaNethmini <himashanethmini05@gmail.com>
2024-05-12 12:23:47 +0530

    added toastify

--- src/components/notification/Notification.jsx
+++ src/components/notification/Notification.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import "./notification.css"
+import { ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css";
 
 const Notification = () => {
   return (
-    <div>Notification</div>
+    <div className=''>
+      <ToastContainer position='bottom-right'/>
+    </div>
   )
 }
 
